2d81bb30c653db9f964ff1557126cbdaff49eb17
polovgrime <polovgrime@gmail.com>
2022-08-29 16:37:47 +0300

    crypto scam landing

---
+++ js/app.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("#goodResultCount").html("&nbsp;" + Math.random() + " вещей сохранено!");
+    $("#badResultCount").html("&nbsp;" + Math.random() + " вещей уничтожено!");
+})
